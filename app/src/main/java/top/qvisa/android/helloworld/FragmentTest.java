bca9b95117a4685791c188d30671ca9f1ceb529a
qvistary <qvistary@gmail.com>
2019-01-26 00:41:24 +0800

    修改为动态添加碎片

--- app/src/main/java/top/qvisa/android/helloworld/FragmentTest.java
+++ app/src/main/java/top/qvisa/android/helloworld/FragmentTest.java
@@ -1,14 +1,46 @@
 package top.qvisa.android.helloworld;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentTransitionImpl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class FragmentTest extends AppCompatActivity {
+import top.qvisa.android.helloworld.Fragment.AnotherRightFragment;
+import top.qvisa.android.helloworld.Fragment.RightFragment;
+
+public class FragmentTest extends AppCompatActivity implements View.OnClickListener {
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_test);
         setTitle("FragmentTest");
+        mButton = findViewById(R.id.l_Fg_Bt_button);//注意是=,不是.
+        mButton.setOnClickListener(this);
+        replaceFragment(new RightFragment());
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.l_Fg_Bt_button://button的点击事件
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
+    }
+
 }
