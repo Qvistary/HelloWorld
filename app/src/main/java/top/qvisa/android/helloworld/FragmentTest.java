27374836984326af25eab3de3115deedcf137b19
qvistary <QINjie023>
2019-01-25 16:32:32 +0800

    Change Main OnclickListener Add FragmentTest

---
+++ app/src/main/java/top/qvisa/android/helloworld/FragmentTest.java
@@ -0,0 +1,14 @@
+package top.qvisa.android.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_test);
+        setTitle("FragmentTest");
+    }
+}
--- app/src/main/java/top/qvisa/android/helloworld/MainActivity.java
+++ app/src/main/java/top/qvisa/android/helloworld/MainActivity.java
@@ -1,13 +1,35 @@
 package top.qvisa.android.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mbfragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mbfragment=findViewById(R.id.Bt_Fragment);
+
+        setOnclickListener();
+    }
+
+    @Override
+    public void onClick(View v) {
+       Intent intent = null;
+        switch (v.getId()) {
+            case R.id.Bt_Fragment:
+                intent = new Intent(MainActivity.this,FragmentTest.class);
+                break;
+
+        }
+        startActivity(intent);
+    }
+
+    public void setOnclickListener(){
+        mbfragment.setOnClickListener(this);
     }
 }
